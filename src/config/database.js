f67ad571310a82542d7124a33a319d6a9b4237d9
magung <agung0401@gmail.com>
2019-09-24 15:04:12 +0700

    update data base

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 'use strict';
 module.exports = {
-    // DB:"mongodb+srv://magung:<password>@cluster0-0u2k7.mongodb.net/test?retryWrites=true&w=majority" ,
-    DB:'mongodb://localhost/ajilapak',
+    DB:'mongodb+srv://ajilapak:ajilapak@cluster0-0u2k7.mongodb.net/test?retryWrites=true&w=majority' ,
+    // DB:'mongodb://localhost/ajilapak',
 };
